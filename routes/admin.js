47cc6b746bfa518c8091598c85923f2a6dd46b5c
Juan Cabello <jcabelloc@itana.pe>
2024-09-09 19:59:58 -0500

    PUG: Uso de Layouts

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,7 @@ const router = express.Router();
 const productos = [];
 
 router.get('/crear-producto', (req, res, next) => {
-    res.sendFile(path.join(raizDir, 'views', 'crear-producto.html'));
+    res.render('crear-producto', { titulo: 'Crear Producto', path: '/admin/crear-producto' });
 });
 
 router.post('/crear-producto', (req, res, next) => {
--- routes/tienda.js
+++ routes/tienda.js
@@ -11,7 +11,7 @@ const router = express.Router();
 // GET requiere una coincidencia exacta en la ruta
 router.get('/', (req, res, next) => {
     const productos = adminData.productos;
-    res.render('tienda', {prods: productos, titulo: 'La Tienda'});
+    res.render('tienda', {prods: productos, titulo: 'La Tienda', path: '/'});
 });
 
 module.exports = router;
